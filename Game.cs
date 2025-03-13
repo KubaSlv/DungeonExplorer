3897e66c1bf68d65e62745b32b9a61c1e33017e1
KubusJeFrytki <kubasliwinski603@gmail.com>
2025-03-13 11:26:24 +0000

    OOP Assessment commit1

--- Game.cs
+++ Game.cs
@@ -27,49 +27,44 @@ namespace DungeonExplorer
                 Console.WriteLine("\n=-=-=-=-=-=-=-\nType 'help' for commands\n=-=-=-=-=-=-=-\nInput: ");   // Ask the player for their input
                 string userInput = Console.ReadLine();
 
-                if (userInput == "help")    // If the player enters 'help', they receive a list of commands which the game works on
+                switch (userInput)
                 {
-                    Console.WriteLine("" +
-                        "Type 'next room' in order to enter the next room" +
-                        "\nType 'pick up' in order to pick up an item " +
-                        "\nType 'inventory' in order to view your inventory " +
-                        "\nType 'description' in order to view room description" +
-                        "\nType 'status' in order to view player status");
-                }
+                    case "help":    // If the player enters 'help', they receive a list of commands which the game works on
+                        Console.WriteLine("" +
+                            "Type 'next room' in order to enter the next room" +
+                            "\nType 'pick up' in order to pick up an item " +
+                            "\nType 'inventory' in order to view your inventory " +
+                            "\nType 'description' in order to view room description" +
+                            "\nType 'status' in order to view player status");
+                        break;
 
-                else if(userInput == "next room")   // If the player enters 'next room' the room and related items are generated
-                {
-                    Console.WriteLine("Entered the next room"); // Inform the player that they've entered the room
-                    currentRoom.RandomRoom();   // Method is called to generate a random room description
-                    player.RandomItem();    // Method is called to generate a random item for the room
-                    Console.WriteLine($"You found {player.GetItem()}"); // Inform the player about the item they've found
-                }
+                    case "next room":   // If the player enters 'next room' the room and related items are generated
+                        Console.WriteLine("Entered the next room"); // Inform the player that they've entered the room
+                        currentRoom.RandomRoom();   // Method is called to generate a random room description
+                        player.RandomItem();    // Method is called to generate a random item for the room
+                        Console.WriteLine($"You found {player.GetItem()}"); // Inform the player about the item they've found
+                        break;
 
-                else if(userInput == "pick up") // If the player enters 'pick up' the item is added to the players inventory
-                {     
+                    case "pick up": // If the player enters 'pick up' the item is added to the players inventory
                         player.PickUpItem(player.GetItem());    // Methods are called which add the item to the players inventory
-                }
+                        break;
 
-                else if(userInput == "inventory")   // If the player enters 'Inventory' the players inventory is displayed
-                {
-                    Console.WriteLine($"Your inventory : {player.InventoryContents()}");    // Display the inventory calling the method which does so
-                }
+                    case "inventory":   // If the player enters 'Inventory' the players inventory is displayed
+                        Console.WriteLine($"Your inventory : {player.InventoryContents()}");    // Display the inventory calling the method which does so
+                        break;
 
-                else if(userInput == "description") // If the player enters 'description' the rooms description is displayed, called by its relevant method
-                {
-                    Console.WriteLine($"Current room description: '{currentRoom.GetDescription()}'");
-                }
+                    case "description": // If the player enters 'description' the rooms description is displayed, called by its relevant method
+                        Console.WriteLine($"Current room description: '{currentRoom.GetDescription()}'");
+                        break;
 
-                else if(userInput == "status")  // If the player enters 'status' their properties are displayed
-                {
-                    Console.WriteLine($"Player status: \nPlayer - {player.Name} \nHealth - {player.Health}");
-                }
+                    case "status":  // If the player enters 'status' their properties are displayed
+                        Console.WriteLine($"Player status: \nPlayer - {player.Name} \nHealth - {player.Health}");
+                        break;
 
-                else   // If none of these inputs are matched, we assume that the player entered an invalid command, and inform them about this, no game logic changes
-                {
-                    Console.WriteLine($"Input '{userInput}' not recognised, try again, or type help for a list of commands");
+                    default:    // If none of these inputs are matched, we assume that the player entered an invalid command, and inform them about this, no game logic changes
+                        Console.WriteLine($"Input '{userInput}' not recognised, try again, or type help for a list of commands");
+                        break;
                 }
-
             }
         }
     }
